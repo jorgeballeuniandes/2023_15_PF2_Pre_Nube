592a7ea7dbc4134f34b8f4ba06c02755071d3557
Jose Manuel Vega <jm.vegaa1@uniandes.edu.co>
2023-10-14 11:59:43 -0500

    firts commit

---
+++ data_bases/init.sql
@@ -0,0 +1,14 @@
+-- Crear la primera base de datos (db1)
+CREATE DATABASE monitor_users;
+
+
+-- Crear la segunda base de datos (db2)
+CREATE DATABASE monitor_users_test;
+CREATE DATABASE monitor_interviews;
+CREATE DATABASE monitor_interviews_test;
+CREATE DATABASE monitor_it_specialists;
+CREATE DATABASE monitor_it_specialists_test;
+CREATE DATABASE monitor_companies;
+CREATE DATABASE monitor_companies_test;
+
+
