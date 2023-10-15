577f49ee460ce27b2d4adccc2586cb35b2df591d
afiffe retamozo <afiffe.retamozo.a@gmail.com>
2023-10-15 11:47:46 -0400

    commit

--- ABCJobs_BK_InterviewsService-main/src/commands/public_create_interview.py
+++ ABCJobs_BK_InterviewsService-main/src/commands/public_create_interview.py
@@ -1,15 +1,15 @@
-from .create_interview import CreateInterview
+ #from .create_interview import CreateInterview
 
-class PublicCreateInterview(BaseCommannd):
-  def __init__(self, data):
-    self.data = data
+ #class PublicCreateInterview(BaseCommannd):
+  # def __init__(self, data):
+     #self.data = data
 
-  def execute(self):
+   #def execute(self):
     #validar  que el usuario tenga relacionada la compañia
     #validateUserHasCompany()
     #validar que proyecto pertenece a la compañia
     #validateCompanyHasProyect()
     #validar que el candidato exista
     #validate
-    interview = CreateInterview(self.data).execute()
-    return interview
+     #interview = CreateInterview(self.data).execute()
+     #return interview
